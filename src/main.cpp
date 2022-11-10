dda78e4761c01c6c04d15244420e881f11f9bb45
Aline Cristina Pinto <alinecristina3.14nto@gmail.com>
2022-11-10 19:21:51 -0300

    feat: adds base structure

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------
+// Arquivo      : main.cpp
+// Conteudo     : Algoritmos de Ordenacao
+// Autor        : Aline Cristina Pinto (alinecristinapinto@ufmg.br)
+//---------------------------------------------------------------------
+
+#include <iostream>
+
+#include "memlog.hpp"
+#include "msgassert.hpp" 
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+   return 0;
+}
